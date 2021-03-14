14932a37d8ac77f82ad83323fa9264e45a4ef893
Admin <admin@Horizon.local>
2021-03-14 22:18:03 +0700

    kick-offๅ

--- src/db/db.py
+++ src/db/db.py
@@ -1,10 +1,21 @@
-import sqlite3 as lite
-from sqlite3 import Error
-from tkinter import *
+import sqlite3
+import time
+import datetime
+import random
 
-con = lite.connect("db/istock.db")
-curs = con.cursor()
-con.commit()
-con.close()
+conn = sqlite3.connect("db/istock.db")
+
+def create_table():
+    c = conn.cursor()
+    c.execute('CREATE TABLE IF NOT EXISTS stuffToPlot (unix REAL, datestamp TEXT, keyword TEXT, value REAL)')
+    c.close()
+
+def data_entry():
+    c = conn.cursor()
+    c.execute("INSERT INTO stuffToPlot VALUES (145123542, '2016-01-03', 'Python', 7)")
+    conn.commit()
+    c.close()
 
 
+create_table()
+data_entry()
