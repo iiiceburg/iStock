7a7141f6ca7b17d8e8026d3d905536ae570f5e68
Peeranat Unhanandana <ounhanan2015@gmail.com>
2021-03-15 01:01:53 +0700

    Database design 100%
    Database design 100%

--- src/db/db.py
+++ src/db/db.py
@@ -1,21 +1,19 @@
-import sqlite3
+import sqlite3 as lite
+
 import time
 import datetime
 import random
 
-conn = sqlite3.connect("db/istock.db")
+dbPath = "db/iStock.db"
+conn = lite.connect("db/iStock.db")
+timeStamp = datetime.datetime.now()
 
-def create_table():
-    c = conn.cursor()
-    c.execute('CREATE TABLE IF NOT EXISTS stuffToPlot (unix REAL, datestamp TEXT, keyword TEXT, value REAL)')
-    c.close()
 
 def data_entry():
     c = conn.cursor()
-    c.execute("INSERT INTO stuffToPlot VALUES (145123542, '2016-01-03', 'Python', 7)")
+    c.execute('INSERT INTO users (username,password,name,email,phone,timestamp) VALUES ("horizon","peeranat123","Peeranat Ounhanan","ounhanan2015@gmail.com","0616454483",timeStamp);')
     conn.commit()
     c.close()
 
-
-create_table()
 data_entry()
+print("Insert success")
