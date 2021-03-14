6db26910ae7d8bc1b89f3a4dc95985dc9af974cd
Admin <admin@Horizon.local>
2021-03-14 22:15:36 +0700

    Add notify and db

--- src/controller/lineNotify.py
+++ src/controller/lineNotify.py
@@ -1,3 +1,11 @@
+import requests
+
 class LineNotify():
     def notify():
-        print("Test connected line notify")
+        url = "https://notify-api.line.me/api/notify"
+        token = "AuQiZeHCkED43oGO1IyoxWax7NOGo7kUqoWxjYBRWsl"
+        headers = {"content-type":"application/x-www-form-urlencoded","Authorization":"Bearer "+token}
+        msg="ผู้ใช้ : Test"
+        req = requests.post(url,headers=headers,data={"message":msg})
+        print(req.text)
+LineNotify.notify()
