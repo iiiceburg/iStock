d7e9f1aaaf077835ce5bcae746b121826c1573ee
fordwoof9 <fordwoof9@outlook.com>
2021-03-25 16:35:20 +0700

    get data (AUTHEN)
    -เปลี่ยน variable.get เป็น entry.get
    -กำหนดชื่อให้ entry

--- src/view/auth.py
+++ src/view/auth.py
@@ -1,5 +1,6 @@
 from tkinter import * 
 import os, sys
+import sqlite3
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 # import ../db/dbConnect.py
 from db.dbConnect import connect_sqLite
@@ -76,31 +77,56 @@ class MainAuthen():
         reg_screen.columnconfigure((0,2),weight = 1)
         reg_screen.resizable(0,0)  
         global reg_username , reg_password , reg_name , reg_email , reg_phone
-        reg_username , reg_password , reg_name , reg_email , reg_phone = StringVar(),StringVar(),StringVar(),StringVar(),StringVar()
+        global username_entry , pass_entry , name_entry , email_entry , phone_entry
+        reg_username = StringVar()
+        reg_password = StringVar()
+        reg_name = StringVar()
+        reg_email = StringVar()
+        reg_phone = StringVar()
         Label(reg_screen,text="CREATE ACCOUNT",fg="black",bg="white",font="vandara 22 bold").grid(row=0,column=1,sticky=N,pady=(50,0))
         Label(reg_screen,text="Username",fg="black",bg="white",font="vandara 10").place(x=325,y=100)
         Label(reg_screen,text="Password",fg="black",bg="white",font="vandara 10").place(x=325,y=170)
         Label(reg_screen,text="Name",fg="black",bg="white",font="vandara 10").place(x=325,y=240)
         Label(reg_screen,text="Email",fg="black",bg="white",font="vandara 10").place(x=325,y=310)
         Label(reg_screen,text="Phone",fg="black",bg="white",font="vandara 10").place(x=325,y=380)
-        Entry(reg_screen,width=20,textvariable=reg_username,bg="lightgrey",fg="black",font="vandara 16 bold",relief='flat',justify=CENTER).grid(row=0,column=1,pady=(120,0),ipady=10,sticky=N)
-        Entry(reg_screen,width=20,textvariable=reg_password,bg="lightgrey",fg="black",font="vandara 16 bold",relief='flat',justify=CENTER).grid(row=0,column=1,pady=(190,0),ipady=10,sticky=N)
-        Entry(reg_screen,width=20,textvariable=reg_name,bg="lightgrey",fg="black",font="vandara 16 bold",relief='flat',justify=CENTER).grid(row=0,column=1,pady=(260,0),ipady=10,sticky=N)
-        Entry(reg_screen,width=20,textvariable=reg_email,bg="lightgrey",fg="black",font="vandara 16 bold",relief='flat',justify=CENTER).grid(row=0,column=1,pady=(330,0),ipady=10,sticky=N)
-        Entry(reg_screen,width=20,textvariable=reg_phone,bg="lightgrey",fg="black",font="vandara 16 bold",relief='flat',justify=CENTER).grid(row=0,column=1,pady=(400,0),ipady=10,sticky=N)
+        username_entry = Entry(reg_screen,width=20, textvariable = reg_username,bg="lightgrey",fg="black",font="vandara 16 bold",relief='flat',justify=CENTER)
+        username_entry.grid(row=0,column=1,pady=(120,0),ipady=10,sticky=N)
+        pass_entry = Entry(reg_screen,width=20, textvariable = reg_password,bg="lightgrey",fg="black",font="vandara 16 bold",relief='flat',justify=CENTER)
+        pass_entry.grid(row=0,column=1,pady=(190,0),ipady=10,sticky=N)
+        name_entry = Entry(reg_screen,width=20, textvariable = reg_name,bg="lightgrey",fg="black",font="vandara 16 bold",relief='flat',justify=CENTER)
+        name_entry.grid(row=0,column=1,pady=(260,0),ipady=10,sticky=N)
+        email_entry = Entry(reg_screen,width=20, textvariable = reg_email,bg="lightgrey",fg="black",font="vandara 16 bold",relief='flat',justify=CENTER)
+        email_entry.grid(row=0,column=1,pady=(330,0),ipady=10,sticky=N)
+        phone_entry = Entry(reg_screen,width=20, textvariable = reg_phone,bg="lightgrey",fg="black",font="vandara 16 bold",relief='flat',justify=CENTER)
+        phone_entry.grid(row=0,column=1,pady=(400,0),ipady=10,sticky=N)
         Button(reg_screen,text="Register",fg="black",bg="orange",font = "vandara 12 bold",width=14,height=2,command=register_insert_data).grid(row=0,column=1,pady=(470,0))
         Button(reg_screen,text="Already have an account? Login",fg="black",bg="white",font = "vandara 10 bold",relief=FLAT,height=2,command=lambda:combine_funcs_already_acc(reg_screen)).grid(row=1,column=1)
         
 
     global register_insert_data
     def register_insert_data():
-        conn = connect_sqLite()
+        
+        get_username = username_entry.get()
+        get_password = pass_entry.get()
+        get_name = name_entry.get()
+        get_email = email_entry.get()
+        get_phone = phone_entry.get()     
+
+        conn = sqlite3.connect('src\db\istock.db')
         curs = conn.cursor()
         reg_insert_data = """INSERT INTO users(username,password,name,email,phone) 
         VALUES(?,?,?,?,?);"""
-        curs.execute(reg_insert_data,(reg_username.get(),reg_password.get(),reg_name.get(),reg_email.get(),reg_phone.get()))
+        curs.execute(reg_insert_data,(get_username,get_password,get_name,get_email,get_phone))
         conn.commit()
         curs.close()
+    
+        print(reg_insert_data)
+
+        print("Username : ",get_username)
+        print("Password : %s"%get_password)
+        print("Name : ",name_entry.get())
+        print("Email : ",get_email)
+        print("Phone : ",get_phone)
 
 
     mainWindow = window()
